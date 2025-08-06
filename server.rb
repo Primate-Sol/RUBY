edcccd74a64f9d74ea9a89bea2e6a0c40f8f279b
Primate-Sol <lotzbattles@gmail.com>
2025-08-06 18:45:42 -0500

    Update server.rb

--- server.rb
+++ server.rb
@@ -1,19 +1,15 @@
 require 'sinatra'
 require 'cowsay'
 
-port = ENV["PORT"] || "8080"
-configure { 
+port = ENV.fetch("PORT", "8080")
+
+configure do 
   set :server, :puma
   set :bind, '0.0.0.0'
   set :port, port
-}
+end
 
 get '/' do
-  content_type "text/plain"
-  message = params[:message]
-  if message == nil
-    message = "Set a message by adding ?message=<message here> to the URL"
-  end
-
-  Cowsay.say(message, "random")
+  content_type :text/plain
+  Cowsay.say(params[:message] || "Set a message by adding ?message=<message here> to the URL", :random)
 end
